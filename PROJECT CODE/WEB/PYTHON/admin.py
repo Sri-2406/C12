24dede79985a45f57ab43e438c260e643389e61a
Sri-2406 <113972102+Sri-2406@users.noreply.github.com>
2023-04-09 14:58:25 +0530

    Create admin.py

---
+++ PROJECT CODE/WEB/PYTHON/admin.py
@@ -0,0 +1,8 @@
+
+# Register your models here.
+from django.contrib import admin
+from . import models
+
+# Register your models here.
+admin.site.register(models.UserPredictDataModel)
+admin.site.register(models.FeedModel)
